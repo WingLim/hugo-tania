9614ceea13e6a46ab8c4351693d09ce94dac0566
WingLim <643089849@qq.com>
2021-05-31 12:13:31 +0800

    feat: switch to enable or disable render footnotes

--- assets/js/features.js
+++ assets/js/features.js
@@ -1,4 +1,10 @@
-renderFootnotes = function () {
+(function(d){
+  let enableFootnotes = false
+  if (d.currentScript) {
+    enableFootnotes = d.currentScript.dataset['enableFootnotes'] ==  'true'
+    console.log(enableFootnotes)
+  }
+  renderFootnotes = function () {
     const removeEl = (el) => {
         if (!el) return;
         el.remove ? el.remove() : el.parentNode.removeChild(el);
@@ -51,7 +57,11 @@ renderFootnotes = function () {
         if (items.length !== 2 || items[0].tagName !== 'HR' || items[1].tagName !== 'OL') return;
         items[1].childElementCount === 0 && removeEl(fn);
     });
-}();
+  };
+  if (enableFootnotes) {
+    renderFootnotes()
+  }
+})(document);
 
 renderAnchor = function () {
     for (let num = 1; num <= 6; num++) {
