9f1603f17d7c64d94b4318b74472edb055efc51d
WingLim <winglim@qq.com>
2021-09-15 13:36:13 +0800

    docs: add copy right

--- assets/ts/copyButton.ts
+++ assets/ts/copyButton.ts
@@ -1,3 +1,6 @@
+// This file is copy from https://github.com/CaiJimmy/hugo-theme-stack/blob/24915a912f23e8c0a21aa156714ea7f071469fdb/assets/ts/main.ts#L61-L87
+// All right reserved by Jimmy Cai
+
 const codeBlocks = document.querySelectorAll('.article-post .highlight');
 const copyText = `Copy`,
     copiedText = `Copied!`;
@@ -10,6 +13,8 @@ export let renderCopyButton = function() {
         codeBlock.appendChild(copyButton);
 
         const pre = codeBlock.getElementsByTagName('pre');
+        // This theme's code block has line number, so the second is where the
+        // real code locate
         const code = pre[1].textContent;
 
         copyButton.addEventListener('click', () => {
